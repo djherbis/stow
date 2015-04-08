90d86029b9cb1ea0c7c6303f82523949139e8f34
djherbis <djherbis@gmail.com>
2015-04-08 19:52:45 -0400

    adding godoc

--- enc.go
+++ enc.go
@@ -7,15 +7,18 @@ import (
 	"io"
 )
 
+// Codec provides a mechanism for storing/retriving objects as streams of data.
 type Codec interface {
 	NewEncoder(io.Writer) Encoder
 	NewDecoder(io.Reader) Decoder
 }
 
+// Encoder is used to encode objects
 type Encoder interface {
 	Encode(interface{}) error
 }
 
+// Decoder is used to decode objects
 type Decoder interface {
 	Decode(interface{}) error
 }
@@ -26,32 +29,41 @@ var (
 	_ Codec = GobCodec{}
 )
 
+// XMLCodec is used to encode/decode XML
 type XMLCodec struct{}
 
+// NewEncoder returns a new xml encoder which writes to w
 func (c XMLCodec) NewEncoder(w io.Writer) Encoder {
 	return xml.NewEncoder(w)
 }
 
+// NewDecoder returns a new xml decoder which reads from r
 func (c XMLCodec) NewDecoder(r io.Reader) Decoder {
 	return xml.NewDecoder(r)
 }
 
+// JSONCodec is used to encode/decode JSON
 type JSONCodec struct{}
 
+// NewEncoder retuns a new json encoder which writes to w
 func (c JSONCodec) NewEncoder(w io.Writer) Encoder {
 	return json.NewEncoder(w)
 }
 
+// NewDecoder returns a new json decoder which reads from r
 func (c JSONCodec) NewDecoder(r io.Reader) Decoder {
 	return json.NewDecoder(r)
 }
 
+// GobCodec is used to encode/decode using the Gob format.
 type GobCodec struct{}
 
+// NewEncoder returns a new gob encoder which writes to w
 func (c GobCodec) NewEncoder(w io.Writer) Encoder {
 	return gob.NewEncoder(w)
 }
 
+// NewDecoder returns a new gob decoder which reads from r
 func (c GobCodec) NewDecoder(r io.Reader) Decoder {
 	return gob.NewDecoder(r)
 }
