ed5cf101e1bfb78d0b402ed2f013a2baecd0fa3e
djherbis <djherbis@gmail.com>
2015-04-08 20:27:48 -0400

    adding more test cases

--- stow_test.go
+++ stow_test.go
@@ -1,30 +1,27 @@
 package stow
 
 import (
+	"encoding/gob"
 	"os"
 	"testing"
 
 	"github.com/boltdb/bolt"
 )
 
-func TestJson(t *testing.T) {
-	db, err := bolt.Open("my.db", 0600, nil)
-	if err != nil {
-		t.Error(err.Error())
-	}
-	defer os.Remove("my.db")
-	defer db.Close()
+type MyType struct {
+	FirstName string `json:"first"`
+	LastName  string `json:"last"`
+}
 
-	type MyType struct {
-		FirstName string `json:"first"`
-		LastName  string `json:"last"`
-	}
+func init() {
+	gob.Register(MyType{})
+}
 
-	store := NewJSONStore(db, []byte("json"))
+func testStore(t *testing.T, store *Store) {
 	store.Put([]byte("hello"), &MyType{"Derek", "Kered"})
 
 	var found bool
-	err = store.ForEach(func(name MyType) {
+	err := store.ForEach(func(name MyType) {
 		found = true
 		if name.FirstName != "Derek" || name.LastName != "Kered" {
 			t.Errorf("mismatching name %s", name)
@@ -45,4 +42,37 @@ func TestJson(t *testing.T) {
 	if name.FirstName != "Derek" || name.LastName != "Kered" {
 		t.Errorf("Unexpected name: %v", name)
 	}
+}
+
+func TestJSON(t *testing.T) {
+	db, err := bolt.Open("my1.db", 0600, nil)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer os.Remove("my1.db")
+	defer db.Close()
+
+	testStore(t, NewJSONStore(db, []byte("json")))
+}
+
+func TestXML(t *testing.T) {
+	db, err := bolt.Open("my2.db", 0600, nil)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer os.Remove("my2.db")
+	defer db.Close()
+
+	testStore(t, NewXMLStore(db, []byte("xml")))
+}
+
+func TestGob(t *testing.T) {
+	db, err := bolt.Open("my3.db", 0600, nil)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer os.Remove("my3.db")
+	defer db.Close()
+
+	testStore(t, NewStore(db, []byte("gob")))
 }
